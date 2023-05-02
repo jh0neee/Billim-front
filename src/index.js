fb0d12e1d4544d5f7d2eaa062b1d4b7176fc5dbd
지현 <jh0neee@naver.com>
2023-05-02 12:34:27 +0900

    style: update index.css and font/color styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import GlobalStyles from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
+import "./index.css";
 import theme from "./styles/theme";
 import App from "./App";
 
