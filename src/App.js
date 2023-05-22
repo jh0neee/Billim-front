9c263d014c377c0b2e5d0808cf2b2f7134639a96
지현 <jh0neee@naver.com>
2023-05-22 23:47:33 +0900

    refactor:Update route configuration based on user authentication status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Reset } from "styled-reset";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
@@ -26,42 +27,68 @@ import FindIdTab from "./components/Auth/FindIdTab.jsx";
 import FindPwTab from "./components/Auth/FindPwTab.jsx";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Routes>
+        <Route path='/new' element={<NewProduct />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<ProductList />}>
+          <Route path='living' element={<ProductList />} />
+          <Route path='apparel' element={<ProductList />} />
+          <Route path='sporting' element={<ProductList />} />
+          <Route path='electronic' element={<ProductList />} />
+        </Route>
+        <Route path='/:itemName/detail' element={<ProductDetail />} />
+        <Route path='/:itemName/payment' element={<ProductPayment />} />
+        <Route path='/mypage' element={<MyPage />}>
+          <Route path='/mypage/purchase' element={<PurchaseManagement />} />
+          <Route path='/mypage/sales' element={<SalesManagement />} />
+          <Route
+            path='/mypage/sales/detail'
+            element={<SalesDetailManagement />}
+          />
+          <Route path='/mypage/favorite' element={<WishList />} />
+          <Route path='/mypage/review' element={<Review />} />
+          <Route path='/mypage/block' element={<BlockManagement />} />
+          <Route path='/mypage/member' element={<EditMember />} />
+          <Route path='/mypage/coupon' element={<MyPageCoupon />} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    );
+  } else {
+    routes = (
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<ProductList />}>
+          <Route path='living' element={<ProductList />} />
+          <Route path='apparel' element={<ProductList />} />
+          <Route path='sporting' element={<ProductList />} />
+          <Route path='electronic' element={<ProductList />} />
+        </Route>
+        <Route path='/:itemName/detail' element={<ProductDetail />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/finduser' element={<FindUser />}>
+          <Route path='/finduser/id' element={<FindIdTab />} />
+          <Route path='/finduser/password' element={<FindPwTab />} />
+        </Route>
+        <Route path='/emailverify' element={<EmailVerification />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<Navigate to='/login' />} />
+      </Routes>
+    );
+  }
+
   return (
     <React.Fragment>
       <Reset />
       <BrowserRouter>
         <ScrollToTop />
         <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<SignIn />} />
-          <Route path='/finduser' element={<FindUser />}>
-            <Route path='/finduser/id' element={<FindIdTab />}/>
-            <Route path='/finduser/password' element={<FindPwTab />}/>
-          </Route>
-          <Route path='/emailverify' element={<EmailVerification />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/product' element={<ProductList />}>
-            <Route path='living' element={<ProductList />} />
-            <Route path='apparel' element={<ProductList />} />
-            <Route path='sporting' element={<ProductList />} />
-            <Route path='electronic' element={<ProductList />} />
-          </Route>
-          <Route path='/new' element={<NewProduct />} />
-          <Route path='/:itemName/detail' element={<ProductDetail />} />
-          <Route path='/:itemName/payment' element={<ProductPayment />} />
-          <Route path='/mypage' element={<MyPage />}>
-            <Route path='/mypage/purchase' element={<PurchaseManagement />} />
-            <Route path='/mypage/sales' element={<SalesManagement />} />
-            <Route path='/mypage/sales/detail' element={<SalesDetailManagement />} />
-            <Route path='/mypage/favorite' element={<WishList />} />
-            <Route path='/mypage/review' element={<Review />} />
-            <Route path='/mypage/block' element={<BlockManagement />} />
-            <Route path='/mypage/member' element={<EditMember />} />
-            <Route path='/mypage/coupon' element={<MyPageCoupon />} />
-          </Route>
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
+        <main>{routes}</main>
       </BrowserRouter>
     </React.Fragment>
   );
