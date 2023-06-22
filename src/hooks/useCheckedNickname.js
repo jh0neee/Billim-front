ee694d2c9e60e0ffc7fed4b8c4f219a446b0e1da
지현 <jh0neee@naver.com>
2023-06-23 03:29:13 +0900

    feat:Implement GET request in checkNickname

--- src/hooks/useCheckedNickname.js
+++ src/hooks/useCheckedNickname.js
@@ -1,23 +1,35 @@
+import axios from 'axios';
 import { toast } from 'react-toastify';
-import { review } from '../data';
 
 export const useCheckNickname = (nickname, setIsCheck) => {
-  const checkNickname = () => {
-    const isCheck = review.map(item => item.username).includes(nickname.value);
+  const url = process.env.REACT_APP_URL;
 
-    if (nickname.value === '') {
-      toast.warning('닉네임을 입력해주세요');
-    } else if (!nickname.isValid) {
-      toast.warning('닉네임은 2~10자로 입력해주세요');
-    } else {
-      if (isCheck) {
-        toast.error('이미 사용중인 닉네임입니다.');
-        setIsCheck(false);
-      } else {
-        toast.success('사용가능한 닉네임입니다.');
-        setIsCheck(true);
-      }
-    }
+  const checkNickname = () => {
+    axios
+      .get(`${url}/member/check/nickname`, {
+        params: { nickname: nickname.value },
+      })
+      .then(response => {
+        console.log(response.data);
+        if (nickname?.value === '') {
+          toast.warning('닉네임을 입력해주세요');
+          setIsCheck(false);
+        } else if (!nickname?.isValid) {
+          toast.warning('닉네임은 2~10자로 입력해주세요');
+          setIsCheck(false);
+        } else {
+          if (response.data) {
+            toast.error('이미 사용중인 닉네임입니다.');
+            setIsCheck(false);
+          } else {
+            toast.success('사용가능한 닉네임입니다.');
+            setIsCheck(true);
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
   };
 
   return [checkNickname];
