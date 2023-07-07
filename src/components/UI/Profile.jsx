50ff80cfb2319a39705d6eb444528052f9b57544
지현 <jh0neee@naver.com>
2023-07-07 23:19:59 +0900

    refactor:Modified code to support circular profile image cropping

--- src/components/UI/Profile.jsx
+++ src/components/UI/Profile.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import profileImage from '../../asset/image/profileImage.png';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  border-radius: 70%;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 export const Profile = props => {
   const { size } = props;
+  const defaultProfileImage =
+    'https://billim.s3.ap-northeast-2.amazonaws.com/profile/profile-default.png';
+
   return (
-    <img src={profileImage} alt="프로필이미지" width={size} height={size} />
+    <>
+      <Box size={size}>
+        <Image src={props.src || defaultProfileImage} alt="프로필이미지" />
+      </Box>
+    </>
   );
 };
