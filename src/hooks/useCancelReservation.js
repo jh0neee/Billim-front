0d624b4d7bb499d23d05b1c040cad5c3b489ec7b
지현 <jh0neee@naver.com>
2023-07-27 17:15:28 +0900

    reservation cancellation test

--- src/hooks/useCancelReservation.js
+++ src/hooks/useCancelReservation.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAuth } from './useAuth';
 
 export const useCancelReservation = initialData => {
+  const url = process.env.REACT_APP_URL;
+  const auth = useAuth();
   const [updatedItem, setUpdatedItem] = useState(initialData);
   const [selectedId, setSelectedId] = useState('');
   const [showReservaionModal, setShowReservationModal] = useState(false);
+
+  useEffect(() => {
+    setUpdatedItem(initialData);
+  }, [initialData]);
 
   const cancelCancellationHandler = () => {
     setShowReservationModal(false);
@@ -15,10 +23,24 @@ export const useCancelReservation = initialData => {
 
   const cancelReservationHandler = () => {
     setShowReservationModal(false);
+
+    const orderItem = updatedItem.find(item => item.orderId === selectedId);
+    const orderId = orderItem.orderId;
+    axios
+      .delete(`${url}/order/${orderId}`, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
     setUpdatedItem(
-      updatedItem.map(item =>
-        item.id === selectedId ? { ...item, status: '취소' } : item,
-      ),
+      updatedItem.map(item => (item.orderId === selectedId ? item : item)),
     );
   };
 
