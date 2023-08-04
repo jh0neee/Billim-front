8de698c2185b0bea73ae0bd879001d18a932df31
지현 <jh0neee@naver.com>
2023-08-04 21:03:06 +0900

    feat:Implement API request in WishList

--- src/components/MyPage/WishList.jsx
+++ src/components/MyPage/WishList.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
+import axios from 'axios';
+import ErrorModal from '../../util/ErrorModal';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import { useAuth } from '../../hooks/useAuth';
+import { useLoadingError } from '../../hooks/useLoadingError';
+import { useTokenRefresher } from '../../hooks/useTokenRefresher';
 import { RiHeart3Fill, RiHeart3Line, RiStarSFill } from 'react-icons/ri';
-import { productItems } from '../../data';
 
 const WishListLayout = styled.div`
   margin: 1rem;
@@ -12,24 +17,46 @@ const WishListLayout = styled.div`
   row-gap: 3rem;
   justify-items: center;
 
-  > * {
-    cursor: pointer;
+  @media (max-width: 1110px) {
+    grid-template-columns: 1.5fr 1.5fr;
+  }
+  @media (max-width: 480px) {
+    grid-template-columns: 1fr;
   }
 `;
 
-const NoneText = styled.p`
+export const NoneText = styled.p`
   text-align: center;
   margin-top: 7rem;
+`;
+
+const Image = styled.img`
+  width: 140px;
+  height: 140px;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+
+  @media (max-width: 1280px) {
+    width: 140px;
+    height: 140px;
+  }
+
+  @media (min-width: 950px) and (max-width: 1110px),
+    (min-width: 640px) and (max-width: 768px),
+    (max-width: 480px) {
+    width: 180px;
+    height: 180px;
+  }
+
+  @media (max-width: 480px) {
+    width: 220px;
+    height: 220px;
+  }
 `;
 
 const ListBox = styled.div`
   display: flex;
   flex-direction: column;
-
-  > p {
-    margin: 0.5rem 0px;
-    font-size: 0.8rem;
-  }
 `;
 
 const ProductItemBox = styled.div`
@@ -37,71 +64,182 @@ const ProductItemBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  > a:hover {
+    text-decoration-line: underline;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 const LikeIcon = styled.div`
   position: absolute;
-  margin-top: 0.4rem;
-  margin-left: 9.2rem;
+  margin-top: 0.3rem;
+  margin-left: 7.3rem;
   font-size: 18px;
+
+  @media (min-width: 950px) and (max-width: 1110px),
+    (min-width: 640px) and (max-width: 768px),
+    (max-width: 480px) {
+    margin-top: 0.4rem;
+    margin-left: 9.7rem;
+    font-size: 20px;
+  }
+
+  @media (max-width: 480px) {
+    margin-top: 0.5rem;
+    margin-left: 11.8rem;
+    font-size: 22px;
+  }
 `;
 
 const ProductParagraph = styled.p`
   font-size: 12px;
+  margin: ${props => (props.amount ? '0 8px' : '0 0 0 0.2rem')};
 
   ${props =>
-    props.amount &&
+    props.productName &&
     css`
-      margin: 0 10px;
+      margin: 0;
+      width: 84px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
     `}
+
+  > span {
+    font-size: 14px;
+    font-weight: 500;
+  }
 `;
 
 const WishList = () => {
-  const [isWishAdd, setIsWishAdd] = useState(false);
-  const favoriteItems = productItems.filter(item => item.likeCount === 1);
+  const url = process.env.REACT_APP_URL;
+  const auth = useAuth();
+  const [interestItems, setInterestItems] = useState([]);
+  const { isLoading, error, onLoading, clearError, errorHandler } =
+    useLoadingError();
+  const { tokenErrorHandler } = useTokenRefresher(auth);
 
-  const handleWishlistClick = item => {
-    setIsWishAdd(!isWishAdd);
+  useEffect(() => {
+    onLoading(true);
+    axios
+      .get(`${url}/product/my/interest`, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
+        params: {
+          memberId: auth.memberId,
+        },
+      })
+      .then(response => {
+        const responseData = response.data.myInterestProductList;
+        setInterestItems(responseData);
+        onLoading(false);
+      })
+      .catch(err => {
+        if (
+          err.response.status === 401 &&
+          err.response.data.code !== 'INVALID_EMAIL_PASSWORD'
+        ) {
+          tokenErrorHandler(err);
+        } else {
+          errorHandler(err);
+        }
+      });
+  }, [auth.token]);
 
-    item.likeCount += 1;
+  const handleInterestToggle = item => {
+    const interestedProductId = item.productId;
+    onLoading(true);
+    const isInterested = interestItems.find(
+      interestItem => interestItem.productId === interestedProductId,
+    );
 
-    if (item.likeCount === 2) {
-      item.likeCount -= 2;
-    }
+    axios
+      .post(
+        `${url}/product/interest`,
+        {
+          interest: !isInterested,
+          productId: interestedProductId,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${auth.token}`,
+          },
+        },
+      )
+      .then(() => {
+        if (isInterested) {
+          setInterestItems(prevItems =>
+            prevItems.filter(
+              interestItem => interestItem.productId !== interestedProductId,
+            ),
+          );
+        } else {
+          setInterestItems(prevItems => [
+            ...prevItems,
+            { productId: interestedProductId },
+          ]);
+        }
+        onLoading(false);
+      })
+      .catch(err => {
+        if (
+          err.response.status === 401 &&
+          err.response.data.code !== 'INVALID_EMAIL_PASSWORD'
+        ) {
+          tokenErrorHandler(err);
+          onLoading(false);
+        } else {
+          errorHandler(err);
+        }
+      });
   };
 
   return (
     <>
+      <ErrorModal error={error} onClear={clearError} />
       <p>관심상품</p>
-      {favoriteItems.length === 0 ? (
+      {isLoading && <LoadingSpinner asOverlay />}
+      {interestItems.length === 0 ? (
         <NoneText>관심상품이 없습니다.</NoneText>
       ) : (
         <WishListLayout>
-          {favoriteItems.map(item => (
-            <ListBox key={item.id}>
-              <Link to={`/${item.name}/detail`}>
-                <img
-                  src="https://via.placeholder.com/169x140"
-                  alt="상품예시이미지"
-                />
+          {interestItems.map(item => (
+            <ListBox key={item.productId}>
+              <Link to={`/${item.productId}/detail`}>
+                <Image src={item.imageUrl} alt="상품예시이미지" />
               </Link>
-              <LikeIcon onClick={() => handleWishlistClick(item)}>
-                {item.likeCount === 1 ? (
+              <LikeIcon onClick={() => handleInterestToggle(item)}>
+                {interestItems.some(
+                  interestItem => interestItem.productId === item.productId,
+                ) ? (
                   <RiHeart3Fill color="red" />
                 ) : (
-                  <RiHeart3Line />
+                  <RiHeart3Line color="grey" />
                 )}
               </LikeIcon>
-              <Link to={`/${item.name}/detail`}>
-                <ProductItemBox>
-                  <ProductParagraph>{item.name}</ProductParagraph>
-                  <ProductParagraph>
-                    <RiStarSFill />
-                    {item.scope}
+              <ProductItemBox>
+                <Link to={`/${item.productId}/detail`}>
+                  <ProductParagraph productName>
+                    {item.productName}
                   </ProductParagraph>
-                </ProductItemBox>
-                <ProductParagraph amount>\ {item.amount} /일</ProductParagraph>
-              </Link>
+                </Link>
+                <div>
+                  <RiStarSFill />
+                  <ProductParagraph>
+                    {(item.starRating || 0).toFixed(1)}
+                  </ProductParagraph>
+                </div>
+              </ProductItemBox>
+              <ProductParagraph amount>
+                <span>{item.price.toLocaleString('kr-KR')}</span> / 일
+              </ProductParagraph>
             </ListBox>
           ))}
         </WishListLayout>
