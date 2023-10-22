ee550925b57ba4d896125a1901209e7a6c36f467
지현 <jh0neee@naver.com>
2023-10-23 00:55:01 +0900

    style: Update style in SignIn

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -8,8 +7,8 @@ import theme from '../../styles/theme';
 import Input from '../../components/UI/Input';
 import Button from '../../components/UI/Button';
 import ErrorModal from '../../util/ErrorModal';
+import kakaoMedium from '../../asset/image/kakao_login_medium_wide.png';
 import LoadingSpinner from '../../components/UI/LoadingSpinner';
-import { ImBubble } from 'react-icons/im';
 import { useForm } from '../../hooks/useForm';
 import { useAuth } from '../../hooks/useAuth';
 import { useLoadingError } from '../../hooks/useLoadingError';
@@ -28,10 +27,6 @@ const SignInLayout = styled.form`
     &:nth-child(2) {
       margin: 1rem 0;
     }
-
-    &:nth-child(3) {
-      margin-top: 1rem;
-    }
   }
 
   @media ${theme.tablet}, ${theme.mobile} {
@@ -39,29 +34,61 @@ const SignInLayout = styled.form`
   }
 `;
 
-const ButtonBox = styled.div`
-  position: relative;
+const LoginButton = styled(Button)`
+  border-radius: 6px;
+  font-size: 13px;
+  font-weight: 400;
+  letter-spacing: 0.5px;
+`;
 
-  > * {
-    &:nth-child(2) {
-      color: black;
-      background: #fee500;
-    }
+const KakaoLoginButton = styled.img`
+  cursor: pointer;
+`;
+
+const DividingLine = styled.span`
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: 18px 0;
+  font-size: 0;
+  line-height: 0;
+
+  &::before {
+    display: inline-block;
+    width: calc(50% - 20px);
+    height: 1px;
+    margin: 8px 0;
+    background-color: rgba(0, 0, 0, 0.06);
+    vertical-align: top;
+    content: '';
+  }
+
+  &::after {
+    display: inline-block;
+    width: calc(50% - 20px);
+    height: 1px;
+    margin: 8px 0;
+    background-color: rgba(0, 0, 0, 0.06);
+    vertical-align: top;
+    content: '';
   }
 `;
 
-const KakaoIcon = styled(ImBubble)`
-  position: absolute;
-  font-size: 17.5px;
-  top: 21px;
-  left: 15px;
+const OrText = styled.span`
+  font-family: 'TRoundWind';
+  letter-spacing: 1.5px;
+  display: inline-block;
+  width: 40px;
+  font-size: 10px;
+  line-height: 18px;
+  text-align: center;
+  color: #828282;
 `;
-
 
 const FindButtonBox = styled.div`
   display: flex;
   align-items: center;
-  margin: 1.5rem 0 0;
+  margin: 0.5rem 0 0;
   font-size: 12px;
 
   > * {
@@ -77,7 +104,7 @@ const FindButtonBox = styled.div`
 const SignUpBox = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 0.5rem;
+  margin-top: 0.8rem;
   font-size: 0.75rem;
 
   > * {
@@ -89,25 +116,22 @@ const SignUpBox = styled.div`
       }
     }
     &:last-child {
-      margin-right: 0.3rem;
-      margin-left: 4rem;
+      margin-right: 0.33rem;
+      margin-left: 3rem;
       cursor: pointer;
       text-decoration: underline;
     }
   }
 `;
 
-
 const SignIn = () => {
   const url = process.env.REACT_APP_URL;
   const auth = useAuth();
   const { isLoading, error, onLoading, clearError, errorHandler } =
-      useLoadingError();
+    useLoadingError();
   const [formState, inputHandler] = useForm({}, false);
 
-  const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API;
-  const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
-  const KAKAO_AUTH_URL = `http://3.36.154.178:8080/oauth2/authorization/kakao`; // redirect - state save,  authorization - find
+  const KAKAO_AUTH_URL = `http://3.36.154.178:8080/oauth2/authorization/kakao`;
 
   const KakaoLoginHandler = () => {
     window.location.href = KAKAO_AUTH_URL;
@@ -117,76 +141,76 @@ const SignIn = () => {
     e.preventDefault();
     onLoading(true);
     axios
-        .post(
-            `${url}/auth/login`,
-            {
-              email: formState.inputs.email.value,
-              password: formState.inputs.password.value,
-            },
-            { headers: { 'Content-Type': 'application/json' } },
-        )
-        .then(response => {
-          if (response.status === 200) {
-            const { accessToken, refreshToken, memberId } = response.data;
-            auth.login(accessToken, refreshToken, memberId);
-          } else {
-            errorHandler(response);
-          }
-          onLoading(false);
-        })
-        .catch(err => {
-          errorHandler(err);
-        });
+      .post(
+        `${url}/auth/login`,
+        {
+          email: formState.inputs.email.value,
+          password: formState.inputs.password.value,
+        },
+        {
+          headers: { 'Content-Type': 'application/json' },
+          withCredentials: true,
+        },
+      )
+      .then(response => {
+        if (response.status === 200) {
+          const { accessToken, refreshToken, memberId } = response.data;
+          auth.login(accessToken, refreshToken, memberId);
+        } else {
+          errorHandler(response);
+        }
+        onLoading(false);
+      })
+      .catch(err => {
+        errorHandler(err);
+      });
   };
 
   return (
-      <>
-        <ErrorModal error={error} onClear={clearError} />
-        <SignInLayout onSubmit={SubmitHandler}>
-          {isLoading && <LoadingSpinner asOverlay />}
-          <Input
-              element="input"
-              id="email"
-              type="text"
-              width="300px"
-              placeholder="Email"
-              validators={[VALIDATOR_REQUIRE()]}
-              errorText="이메일을 입력해주세요"
-              onInput={inputHandler}
-          />
-          <Input
-              element="input"
-              id="password"
-              type="password"
-              width="300px"
-              placeholder="Password"
-              validators={[VALIDATOR_REQUIRE()]}
-              errorText="비밀번호를 입력해주세요"
-              onInput={inputHandler}
-          />
-          <Button type="submit" disabled={!formState.isValid}>
-            로그인
-          </Button>
-          <ButtonBox>
-            <KakaoIcon />
-            <Button type="button" onClick={KakaoLoginHandler}>
-              카카오 로그인
-            </Button>
-          </ButtonBox>
-          <FindButtonBox>
-            <p>비밀번호를 잊어버리셨나요?</p>
-            <Link to="/finduser/password">비밀번호 찾기</Link>
-          </FindButtonBox>
-          <SignUpBox>
-            <p>
-              빌림의 회원이 되시면 <span>할인쿠폰</span>을 <br /> 드립니다.
-            </p>
-            <Link to="/emailverify">회원가입</Link>
-          </SignUpBox>
-        </SignInLayout>
-      </>
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <SignInLayout onSubmit={SubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          element="input"
+          id="email"
+          type="text"
+          width="300px"
+          placeholder="Email"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="이메일을 입력해주세요"
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          width="300px"
+          placeholder="Password"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="비밀번호를 입력해주세요"
+          onInput={inputHandler}
+        />
+        <LoginButton type="submit" disabled={!formState.isValid}>
+          로그인
+        </LoginButton>
+        <FindButtonBox>
+          <p>비밀번호를 잊어버리셨나요?</p>
+          <Link to="/finduser/password">비밀번호 찾기</Link>
+        </FindButtonBox>
+        <SignUpBox>
+          <p>
+            회원이 되시면 <span>할인쿠폰</span>을 드립니다.
+          </p>
+          <Link to="/emailverify">회원가입</Link>
+        </SignUpBox>
+        <DividingLine>
+          <OrText>OR</OrText>
+        </DividingLine>
+        <KakaoLoginButton src={kakaoMedium} onClick={KakaoLoginHandler} />
+      </SignInLayout>
+    </>
   );
 };
 
 export default SignIn;
-
