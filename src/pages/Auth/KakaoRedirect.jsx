6e786bd39c00f4a177c058f202ef4c87c814d15f
지현 <jh0neee@naver.com>
2023-10-30 10:39:19 +0900

    style:Reformat and clean up KakaoRedirect component

--- src/pages/Auth/KakaoRedirect.jsx
+++ src/pages/Auth/KakaoRedirect.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -8,37 +7,33 @@ import { useAuth } from '../../hooks/useAuth';
 import { useLoadingError } from '../../hooks/useLoadingError';
 
 const KakaoRedirect = () => {
-    const auth = useAuth();
-    const location = useLocation();
-    const navigate = useNavigate();
-    const searchParams = new URLSearchParams(location.search);
-    const { isLoading, onLoading } = useLoadingError();
+  const auth = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const searchParams = new URLSearchParams(location.search);
+  const { isLoading, onLoading } = useLoadingError();
 
-    const code = searchParams.get('code');
-    const state = searchParams.get('state');
+  const code = searchParams.get('code');
+  const state = searchParams.get('state');
 
-    useEffect(() => {
-        console.log(code, state);
-        onLoading(true);
-        axios
-            .get(`http://localhost:3000/oauth/kakao?code=${code}&state=${state}`)
-            .then(response => {
-                console.log(response.data);
-                const { accessToken, refreshToken, memberId } = response.data;
-                auth.login(accessToken, refreshToken, memberId);
-                navigate('/');
-                onLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-                onLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    onLoading(true);
+    axios
+      .get(`http://localhost:3000/oauth/kakao?code=${code}&state=${state}`)
+      .then(response => {
+        const { accessToken, refreshToken, memberId } = response.data;
+        auth.login(accessToken, refreshToken, memberId);
+        navigate('/');
+        onLoading(false);
+      })
+      .catch(() => {
+        onLoading(false);
+      });
+  }, []);
 
-    if (isLoading) {
-        return <LoadingSpinner />;
-    }
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 };
 
 export default KakaoRedirect;
-
