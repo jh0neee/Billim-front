94b54db121978b7a5fdf655852fe26d311a491b0
지현 <jh0neee@naver.com>
2023-10-31 22:22:33 +0900

    feat:Implement lazy loading for React code splitting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Reset } from 'styled-reset';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from './pages/Home.jsx';
 import Header from './components/Navigation/Header.jsx';
-import SignIn from './pages/Auth/SignIn.jsx';
-import FindUser from './pages/Auth/FindUser.jsx';
-import SignUp from './pages/Auth/SignUp.jsx';
-import EmailVerification from './pages/Auth/EmailVerification.jsx';
-import ProductList from './pages/Product/ProductList.jsx';
-import ProductDetail from './pages/Product/ProductDetail.jsx';
-import ProductPayment from './pages/Product/ProductPayment.jsx';
 import ScrollToTop from './util/ScrollToTop.js';
-import MyPage from './pages/MyPage.jsx';
-import PurchaseManagement from './components/MyPage/PurchaseManagement.jsx';
-import SalesManagement from './components/MyPage/SalesManagement.jsx';
-import WishList from './components/MyPage/WishList.jsx';
-import SalesDetailManagement from './components/MyPage/SalesDetailManagement.jsx';
-import Review from './components/MyPage/Review.jsx';
-import EditMember from './components/MyPage/EditMember.jsx';
-import NewProduct from './pages/NewProduct.jsx';
-import MyPageCoupon from './components/MyPage/MyPageCoupon.jsx';
-import UpdateProduct from './pages/Product/UpdateProduct.jsx';
-import Chat from './pages/Chat.jsx';
-import MessageChat from './components/Chat/MessageChat.jsx';
-import BlockChat from './components/Chat/BlockChat.jsx';
-import EmailConfirm from './pages/Auth/EmailConfirm.jsx';
 import BackLocation from './util/BackLocation.js';
-import KakaoRedirect from './pages/Auth/KakaoRedirect.jsx';
+import LoadingSpinner from './components/UI/LoadingSpinner.jsx';
+const Home = React.lazy(() => import('./pages/Home.jsx'));
+const Chat = React.lazy(() => import('./pages/Chat.jsx'));
+const SignIn = React.lazy(() => import('./pages/Auth/SignIn.jsx'));
+const SignUp = React.lazy(() => import('./pages/Auth/SignUp.jsx'));
+const MyPage = React.lazy(() => import('./pages/MyPage.jsx'));
+const Review = React.lazy(() => import('./components/MyPage/Review.jsx'));
+const WishList = React.lazy(() => import('./components/MyPage/WishList.jsx'));
+const FindUser = React.lazy(() => import('./pages/Auth/FindUser.jsx'));
+const BlockChat = React.lazy(() => import('./components/Chat/BlockChat.jsx'));
+const NewProduct = React.lazy(() => import('./pages/NewProduct.jsx'));
+const ProductList = React.lazy(() => import('./pages/Product/ProductList.jsx'));
+const EmailConfirm = React.lazy(() => import('./pages/Auth/EmailConfirm.jsx'));
+const EditMember = React.lazy(() =>
+  import('./components/MyPage/EditMember.jsx'),
+);
+const MessageChat = React.lazy(() =>
+  import('./components/Chat/MessageChat.jsx'),
+);
+const MyPageCoupon = React.lazy(() =>
+  import('./components/MyPage/MyPageCoupon.jsx'),
+);
+const KakaoRedirect = React.lazy(() =>
+  import('./pages/Auth/KakaoRedirect.jsx'),
+);
+const UpdateProduct = React.lazy(() =>
+  import('./pages/Product/UpdateProduct.jsx'),
+);
+const ProductDetail = React.lazy(() =>
+  import('./pages/Product/ProductDetail.jsx'),
+);
+const ProductPayment = React.lazy(() =>
+  import('./pages/Product/ProductPayment.jsx'),
+);
+const SalesManagement = React.lazy(() =>
+  import('./components/MyPage/SalesManagement.jsx'),
+);
+const EmailVerification = React.lazy(() =>
+  import('./pages/Auth/EmailVerification.jsx'),
+);
+const PurchaseManagement = React.lazy(() =>
+  import('./components/MyPage/PurchaseManagement.jsx'),
+);
+const SalesDetailManagement = React.lazy(() =>
+  import('./components/MyPage/SalesDetailManagement.jsx'),
+);
 
 function App() {
   const token = useSelector(state => state.auth.token);
@@ -96,7 +119,9 @@ function App() {
         <ScrollToTop />
         <BackLocation />
         <Header />
-        <main>{routes}</main>
+        <main>
+          <Suspense fallback={<LoadingSpinner />}>{routes}</Suspense>
+        </main>
       </BrowserRouter>
     </React.Fragment>
   );
